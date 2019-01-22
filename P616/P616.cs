f677edbf8a02f5e1ab44e8440b77e04b668eb50f
James <jamesnicholasmartin@gmail.com>
2019-01-22 08:22:13 -0800

    Initial commit

---
+++ P616/P616.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P616
+{
+    class P616
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
